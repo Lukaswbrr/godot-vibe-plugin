a4bb922b3687e216e78a76bee072ab766690bc2b
Lukaswbrr <49600807+Lukaswbrr@users.noreply.github.com>
2026-03-06 10:07:48 -0300

    update readme instruction, testing instructions openai

--- README.md
+++ README.md
@@ -4,7 +4,17 @@ A Vibe Coding Assistent for Godot Engine
 
 ## Instructions
 
-First, run the FastAPI server using the fastapi command in the terminal (assuming you're inside the backend folder):
+### FastAPI
+
+Before getting started, you must create a [Github Personal Token](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens) to access the AI model from [Github Models](https://github.com/marketplace?type=models).
+
+Once you created your personal token, create a .env file with the following:
+
+```text
+GITHUB_TOKEN="YOUR_TOKEN_HERE"
+```
+
+Then, run the FastAPI server using the fastapi command in the terminal (assuming you're inside the backend folder):
 
 ```bash
 fastapi dev main.py
--- backend/test_openai.py
+++ backend/test_openai.py
@@ -13,12 +13,36 @@ client = OpenAI(
     api_key=token,
 )
 
+response_style = """Return your answer in raw Json format instructions for Godot Engine editor menu (used as a EditorScript).
+
+The values of the parameters can be a string, int, float, boolean, list or dictionary, depending on the context of the instruction.
+
+The JSON should have the following structure: 
+{
+    "instruction": {
+        "parameter1": "value 1",
+        "parameter2": "value 2",
+        ...
+    }
+}
+Replace instruction with the name of the instruction you want to execute and add the necessary parameters for that instruction.
+
+The possible instructions are:
+- "print": Print a message to the console. The parameter is "message" (string).
+- "create_node": Create a new node in the scene tree. The parameters are "node_type" (string) and "node_name" (string).
+- "remove_node": Remove a node in the scene tree. The parameters are "node_name" (string).
+"""
+
+json_response_structure = {
+    "answer": "your answer here"
+}
+
 def test_chat(message: str = "What is Godot Engine?") -> str:
     response = client.chat.completions.create(
         messages=[
         {
             "role": "system",
-            "content": "You are a helpful assistant.",
+            "content": response_style,
         },
         {
             "role": "user",
